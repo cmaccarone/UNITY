287cc3cb0a617586fd982727de537d38c4020eda
cm4c <cm4c>
2019-04-26 16:57:09 -0700

    more UI touch-ups

---
+++ UNITY/Controllers/SignInVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UNITY
+//
+//  Created by Caleb Maccarone on 4/26/19.
+//  Copyright © 2019 Caleb Maccarone. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignInVC: UIViewController {
+    //vars
+    var placeHolder: String?
+    //outlets/Actions
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var emailTextField: RoundedTextField!
+    
+    @IBOutlet weak var passwordTextField: RoundedTextField!
+    
+    @IBAction func signInWithTouchID(_ sender: Any) {
+    }
+    
+    @IBAction func signInRegular(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+   
+    
+    
+}
+
