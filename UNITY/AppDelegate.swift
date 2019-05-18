4af985c4c8bc2555778bbdbd13ac321df0ea9a33
cm4c <cm4c>
2019-05-17 22:08:23 -0700

    new

--- UNITY/AppDelegate.swift
+++ UNITY/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
+//update github
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
--- UNITY/Controllers/ResetPasswordVC1.swift
+++ UNITY/Controllers/ResetPasswordVC1.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class ResetPasswordVC1: UIViewController, UITextFieldDelegate {
 
